8b6eca4f427e446665b6f8c505e87def9369854d
Rüdiger Schulz <rueschu@gmail.com>
2019-08-12 13:55:26 +0200

    Code from lesson 1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
-// application
+const model = {
+    company: 'Mister Spex',
+    name: '',
+    salaray: 0,
+    list: [
+        {
+            day: '',
+            startTime: '',
+            endTime: '',
+            breakDuration: ''
+        }
+    ]
+};
+
+const hours = 8.0;
+
+const heading = document.querySelector('h1');
+const nameField = document.querySelector('#TTNameSurname');
+const appOutput = document.querySelector('#TTHours');
+
+const render = (model) => {
+    heading.innerText = `${model.company} Time Sheet${model.name !== '' ? ` for ${model.name}` : ''}`;
+    appOutput.innerText = `${hours} Hours`;
+};
+
+const onNameFieldChanged = (event) => {
+    const newName = event.target.value;
+    model.name = newName;
+    console.log(`name field was changed to ${newName}`);
+    render(model);
+};
+
+const onSalaryFieldChanged = (event) => {
+    const newSalaray = event.target.value;
+    model.salaray = newSalaray;
+    console.log(`salary field was changed to ${newSalaray}`);
+    render(model);
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-    window.alert('Hello, Mister Spex!');
+    console.log('Hello, Mister Spex!');
+    console.log(`Heading is ${heading.innerText}.`);
+
+    nameField.addEventListener('keyup', onNameFieldChanged);
+
+    render(model);
 });
